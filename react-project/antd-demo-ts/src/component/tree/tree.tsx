c5a97cee906f45140ff1435ac0a9004ea6405b02
Liang yg <38033445+GenXiaoLe@users.noreply.github.com>
2019-11-27 18:07:22 +0800

    Update tree.tsx

--- react-project/antd-demo-ts/src/component/tree/tree.tsx
+++ react-project/antd-demo-ts/src/component/tree/tree.tsx
@@ -1,59 +1,91 @@
-import * as React from 'react';
-import { Icon, Tree as AntTree } from 'antd';
-
-import './tree.css';
+import React from 'react';
+import { Tree as AntTree } from 'antd';
+import SearchTree from './searchTree';
 
 const { TreeNode } = AntTree;
 
-export interface Props {
-    data: Array<any>
-    isListData?: boolean
+export interface DefaultPropsTree {
+    /** 子元素递归id依据字段, 例如: goodsId, id, companyId */
+    id: string;
+    /** 节点显示的名称 */
+    CNname: string;
 }
 
-export interface State {
-    
+export interface TreeDataSource {
+    parentId: number;
+    [key: string]: any
 }
 
-export default class Tree extends React.Component<Props, State> {
-    constructor(props: Props) {
+interface TreeProps {
+    dataSource: TreeDataSource[];
+    defaultPropsTree: DefaultPropsTree;
+    onChange: (selectedKeys: string[]) => void;
+    isList?: boolean
+} 
+
+export default class Tree extends React.Component<TreeProps, {}> {
+    public static SearchTree: typeof SearchTree;
+    public static DefaultPropsTree: DefaultPropsTree;
+    public static TreeDataSource: TreeDataSource;
+
+    public constructor(props: TreeProps) {
         super(props);
-
-        this.createTree = this.createTree.bind(this);
     }
 
-    createTree = (data: Array<any>, id: number = 0, attribute: string = 'parent_id'): Array<any> => {
-        return data
-            .filter((item: any, index: number) => id === item[attribute])
-            .map(item => {
-                return {...item, children: this.createTree(data, item.id)};
-            });
+    // 树状循环
+    public treeDataFn = (list: TreeDataSource[], parentId: number) => {
+        let items: any[] = [];
+        for (let i = 0; i < list.length; i++) {
+            let key = list[i].parentId;
+            if (items[key]) {
+                items[key].push(list[i]);
+            } else {
+                items[key] = [];
+                items[key].push(list[i]);
+            }
+        }
+        return this.formatTree(items, parentId);
     }
 
-    renderTreeNodes = (data: Array<any>) => {
-        return data.map((item) => {
+    // 格式化数据
+    public formatTree = (items: any[], parentId: number) => {
+        const { defaultPropsTree } = this.props;
+        let result: any[] = [];
+        if (!items[parentId]) {
+            return result;
+        }
+        for (let t of items[parentId]) {
+            t.children = this.formatTree(items, t[defaultPropsTree.id]);
+            result.push(t);
+        }
+        return result;
+    }
+
+    public renderTreeNodes = (data: TreeDataSource[]): Array<React.ReactNode> => {
+        const { defaultPropsTree } = this.props;
+
+        return data.map((item: TreeDataSource) => {
             if (item.children) {
                 return (
-                    <TreeNode title={item.title} key={item.id} dataRef={item}>
+                    <TreeNode title={item[defaultPropsTree.CNname]} key={item[defaultPropsTree.id]} dataRef={item}>
                         {this.renderTreeNodes(item.children)}
                     </TreeNode>
                 )
             }
 
-            return <TreeNode title={item.title} key={item.id} {...item}></TreeNode>
-        })
+            return <TreeNode title={item[defaultPropsTree.CNname]} key={item[defaultPropsTree.id]} {...item} />
+        });
     }
 
-    render() {
-        const { data, isListData = true } = this.props;
-        const _tree: Array<any> =  isListData ? this.createTree(data) : data;
-        
-        return(
-            <section>
-                <AntTree>
-                    {this.renderTreeNodes(_tree)}
-                </AntTree>
-            </section>
+    public render() {
+        const { dataSource, onChange, isList = true } = this.props;
+        let _tree: TreeDataSource[] = isList ? this.treeDataFn(dataSource, 0) : dataSource;
+
+        return (
+            <AntTree
+                onSelect={onChange}>
+                {this.renderTreeNodes(_tree)}
+            </AntTree>
         )
     }
 }
-
